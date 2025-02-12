c43159757cd5f3601000051ae402fcec6226995e
diyakrishnatk <136306065+diyakrishnatk@users.noreply.github.com>
2025-02-12 11:01:50 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers{
+public static void main(String[] args){
+int a=-1;
+if(a>0){
+System.out.println("it is positive number");
+}else if (a==0){
+System.out.println("it is zero");
+}
+else{
+System.out.println("it is a negative number");
+}
+}
+}
