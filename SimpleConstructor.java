8dd8d8b8fe1cb346102540e6f1fc0493eb1b0e6e
diyakrishnatk <136306065+diyakrishnatk@users.noreply.github.com>
2025-03-05 10:58:14 +0530

    Add files via upload

---
+++ SimpleConstructor.java
@@ -0,0 +1,8 @@
+public class SimpleConstructor{
+SimpleConstructor(){
+System.out.println("this is an example program for understanding simple constructor");
+}
+public static void main(String args[]){
+SimpleConstructor sim= new SimpleConstructor();
+}
+} 
